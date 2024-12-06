b7f3a6bb617ab05eb0382feeba4040ffeadb41cd
Albin Aji <albinpariyarathu@gmail.com>
2024-12-06 07:38:06 +0530

    toast bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import AppointmentManager from "./components/AppointmentManager";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
